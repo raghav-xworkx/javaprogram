03e2d15a0ca1a05dc00f3ffb93541f68b41f83c2
raghav-xworkx <raghav.xworkz@gmail.com>
2023-07-19 22:14:16 -0800

    Java codes

---
+++ AllInOne.java
@@ -0,0 +1,16 @@
+public class AllInOne {
+    public static void main(String[] arg) {
+        Soda.bottle("Abdul soda", "Kinly Soda", "Normal soda");
+        System.out.println("/n");
+        WeightMachine.weight("Machine 1", "Machine 2", "Machine 3");
+        System.out.println("/n");
+        Kulfi.coldice("Arun Kulfi", "Nandini Kulfi", "Butter kulfi");
+        System.out.println("/n");
+        Owen.fire("Baking Oven", "BakingTempInc", "Oven form");
+        System.out.println("/n");
+        GoldSmith.smith("Gold Smith 1", "Gold Smith 2", "Gold Smith 3");
+        Stove.onfire("Stove 1", "Stove 2", "Stove 3");
+        System.out.println("/n");
+
+    }
+}
